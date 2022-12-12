abd0a4a4dd6921f4990b1cd0882868b07b2e93f3
koba <kenichiro.kobayashi@stockmark.co.jp>
2022-12-12 17:08:18 +0900

    SoC対応

--- pdf2struct_debug.py
+++ pdf2struct_debug.py
@@ -1,7 +1,7 @@
 import json
 from pdf_split import pdf2struct_data
 
-ret = pdf2struct_data("JSoC1319.pdf")
+ret = pdf2struct_data("JSoC1299.pdf")
 json_file = open("test.json", mode="w")
 json.dump(ret, json_file, indent=2, ensure_ascii=False)
 json_file.close()
--- pdf_split.py
+++ pdf_split.py
@@ -40,7 +40,7 @@ def pdf2text(file):
 
 def english_chek(text):
     for item in text:
-        if item.isalpha():
+        if item.isalpha() and ("腕" <= item or item <= "ぁ"):
             continue
         elif not item.isascii():
             cld3_languages = cld3.get_frequent_languages(text, num_langs=3, )
@@ -65,8 +65,7 @@ def space_cut(text):
             continue
         if ch == " " or ch == " ":
             continue
-        if ("A" <= ch <= "~" or "!" <= ch <= "?" or ch == "£") and ret and (
-                "A" <= ret[-1] <= "~" or "!" <= ret[-1] <= "?" or ret[-1] == "£"):
+        if ("A" <= ch <= "~" or "!" <= ch <= "?" or ch == "£") and ret and ("A" <= ret[-1] <= "~" or "!" <= ret[-1] <= "?" or ret[-1] == "£"):
             ret = ret + " " + ch
         else:
             ret = ret + ch
@@ -75,7 +74,12 @@ def space_cut(text):
         ret = ret + " "
     return ret
 
-
+def alpha_count(text):
+    ret = 0
+    for ch in text:
+        if "!" <= ch <= "~":
+            ret = ret + 1
+    return ret
 def p_report_split(file_name):
     ret = pdf2text(file_name)
     print(ret)
@@ -84,8 +88,8 @@ def p_report_split(file_name):
     SoClink = []
     link = []
     not_connect = True
-    SoCtopic = False
-    Ptopic = False
+    soctopic = False
+    p_topic = False
     abstract = ""
     explanation = ""
     caption_english = ""
@@ -103,6 +107,9 @@ def p_report_split(file_name):
         if "id" not in result and line[0] == "P":
             result["id"] = line
             continue
+        if "date" not in result and line and "年" in line:
+            result["date"] = line
+            continue
         if "title_english" not in result and line:
             result["title_english"] = line
             continue
@@ -110,9 +117,6 @@ def p_report_split(file_name):
             result["title"] = line
             continue
         if "Relevance" in line:
-            continue
-        if "date" not in result and line:
-            result["date"] = line
             continue
         if line.startswith("Timing"):
             continue
@@ -135,12 +139,13 @@ def p_report_split(file_name):
             if explanation:
                 topic_data["explanation"] = explanation
                 related_topic.append(topic_data)
+                topic_data = {}
                 result["document"]["topic"]["related_topic"] = related_topic
                 explanation = ""
-            SoCtopic = True
+            soctopic = True
             continue
         if line.startswith("関連する"):
-            Ptopic = True
+            p_topic = True
             continue
         not_connect = False
         if line.startswith("SC-") and "abstract" not in result["document"]["topic"]:
@@ -167,7 +172,13 @@ def p_report_split(file_name):
                 explanation = ""
             continue
         if caption_english and english_chek(line):
-            caption_english = caption_english + " " + line
+            if caption_english.endswith("- "):
+                if line[0].isupper():
+                    caption_english = caption_english[:-1] + line
+                else:
+                    caption_english = caption_english[:-2] + line
+            else:
+                caption_english = caption_english + line
             continue
         elif len(caption_english) > 1:
             topic_data["id"] = caption_english.split("—")[0]
@@ -176,9 +187,9 @@ def p_report_split(file_name):
             explanation = explanation + line
         elif explanation:
             explanation = explanation + line
-        if line.startswith("SoC") and SoCtopic:
+        if line.startswith("SoC") and soctopic:
             SoClink.append(org_line)
-        if line.startswith("P") and Ptopic:
+        if line.startswith("P") and p_topic:
             link.append(org_line)
     result["soc_link"] = SoClink
     result["partners_link"] = link
@@ -197,16 +208,30 @@ def jsoc_report_split(file_name):
     caption = ""
     captino_connect = False
     caption_ok = False
-    SoCtopic = False
+    captino_long = False
+    null_caption = False
+    soctopic = False
+    explanation_ok = False
     summary_start = False
+    summary_find = False
     summary = ""
+    summary_len = 15
+    max_len = 0
     last_len = 0
     result = {}
     result["document"] = {}
     result["document"]["topic"] = {}
+    doc = []
     for org_line in ret.splitlines():
+        doc.append(org_line)
+    ct = -1
+    for org_line in doc:
+        ct = ct + 1
         line = space_cut(org_line)
-        if "date" not in result and line:
+        if alpha_count(line) == 0:
+            if len(line) > max_len:
+                max_len = len(line)
+        if "date" not in result and line and "年" in line:
             result["date"] = line
             continue
         if "id" not in result and line and line[0] == "S":
@@ -221,52 +246,7 @@ def jsoc_report_split(file_name):
         if "title" not in result and line:
             result["title"] = line
             continue
-        if "abstract" not in result["document"]["topic"] and "title" in result and not abstract:
-            if len(line) <= 2:
-                continue
-            abstract = line
-            continue
-        if line.startswith("SoC") and not SoCtopic:
-            if explanation:
-                exp = explanation.strip('\r\n')
-                ex_l = ""
-                for ll in exp.split("。"):
-                    if len(ll) > 2:
-                        ex_l = ex_l + ll + "。\n"
-                topic_data["explanation"] = ex_l
-                related_topic.append(topic_data)
-                result["document"]["topic"]["related_topic"] = related_topic
-                explanation = ""
-            continue
-        if line.startswith("本トピックスに関連する"):
-            SoCtopic = True
-            continue
-        if line.startswith("(cid:") and "abstract" not in result["document"]["topic"]:
-            ab_l = ""
-            for ll in abstract.split("。"):
-                if len(ll) > 2:
-                    ab_l = ab_l + ll + "。\n"
-            abstract = ""
-            result["document"]["topic"]["abstract"] = ab_l
-        if len(line) < 14 and not line.startswith("(cid:"):
-            if len(line) < 2:
-                if not summary_start and not summary:
-                    summary_start = True
-                else:
-                    summary_start = False
-                if not caption:
-                    continue
-            if summary_start:
-                summary = summary + line
-                if not caption:
-                    continue
-        else:
-            summary_start = False
-        if abstract:
-            abstract = abstract + line
-            continue
-        if line.startswith("(cid:"):
-            caption = line
+        if line.startswith("SoC") and not soctopic:
             if explanation:
                 exp = explanation.strip('\r\n')
                 ex_l = ""
@@ -277,46 +257,157 @@ def jsoc_report_split(file_name):
                 related_topic.append(topic_data)
                 topic_data = {}
                 explanation = ""
-            if "cid:122" in line:
+            if "abstract" not in result["document"]["topic"]:
+                ab_l = ""
+                for ll in abstract.split("。"):
+                    if len(ll) > 2:
+                        ab_l = ab_l + ll + "。\n"
+                abstract = ""
+                result["document"]["topic"]["abstract"] = ab_l
+            result["document"]["topic"]["related_topic"] = related_topic
+            continue
+        if line.startswith("本トピックスに関連する"):
+            soctopic = True
+            continue
+        if line.startswith("(cid:") and "abstract" not in result["document"]["topic"]:
+            ab_l = ""
+            for ll in abstract.split("。"):
+                if len(ll) > 2:
+                    ab_l = ab_l + ll + "。\n"
+            abstract = ""
+            result["document"]["topic"]["abstract"] = ab_l
+        if len(line) - alpha_count(line)/2 < summary_len and not line.startswith("(cid:") and "title" in result and not caption_ok:
+            if len(line) < 1:
+                if not summary_start and not summary:
+                    summary_start = True
+                elif len(summary) < 20:
+                    summary_start = False
+                    summary = ""
+                else:
+                    summary_start = False
+                    if summary:
+                        summary_find = True
+                    if abstract and abstract[-1] != "\n":
+                        abstract = abstract + "\n"
+                if not caption:
+                    continue
+            if summary_start:
+                summary = summary + line
+                if not caption:
+                    continue
+        else:
+            if summary and abstract and not summary_find:
+                abstract = abstract + "\n" + summary
+                summary = ""
+            if caption_ok and not summary_find:
+                summary = ""
+            summary_start = False
+        if "abstract" not in result["document"]["topic"] and "title" in result and not abstract:
+            if len(line) <= 2:
+                continue
+            abstract = line
+            continue
+        if line.startswith("(cid:") or line.startswith("◆") or line.startswith(""):
+            if line.startswith("◆") or line.startswith(""):
+                line = "(cid:139)" + line[1:]
+            if len(line) < 11:
+                null_caption = True
+            explanation_ok = False
+            if caption:
+                if "cid:190" in line and not explanation:
+#                    topic_data["id"] = caption.split(")")[0] + ")"
+                    topic_data["caption"] = caption.split(")")[-1]
+                    explanation = line.split(")")[-1]
+                    caption = ""
+                    caption_ok = False
+                    captino_connect = False
+                    continue
+                else:
+#                    topic_data["id"] = caption.split(")")[0] + ")"
+                    topic_data["caption"] = caption.split(")")[-1]
+                    topic_data["explanation"] = ""
+                    related_topic.append(topic_data)
+                    topic_data = {}
+                    caption_ok = False
+                    captino_connect = False
+            caption = line
+#            if explanation and "cid:122" not in line and "cid:190" not in line:
+            if explanation:
+                exp = explanation.strip('\r\n')
+                ex_l = ""
+                for ll in exp.split("。"):
+                    if len(ll) > 2:
+                        ex_l = ex_l + ll + "。\n"
+                topic_data["explanation"] = ex_l
+                related_topic.append(topic_data)
+                topic_data = {}
+                explanation = ""
+            ll = len(line)
+            if len(line) >= max_len:
+                captino_long = True
+            else:
+                captino_long = False
+            """
+            if "cid:122" in line or "cid:190" in line:
+                explanation = explanation + "\n・ " + line.split(")")[-1]
+            """
+            if "cid:122" in line or "cid:190" in line:
                 caption_less = True
-                topic_data["id"] = line.split(")")[0] + ")"
-                topic_data["caption"] = ""
+#                topic_data["id"] = line.split(")")[0] + ")"
+                if "caption" not in topic_data:
+                    topic_data["caption"] = ""
                 caption_ok = False
                 captino_connect = False
+                captino_long = False
                 explanation = line.split(")")[-1]
                 caption = ""
             continue
-        if caption and len(line) < 2 and "caption" not in topic_data:
+        if caption and len(line) < 1 and "caption" not in topic_data:
             if captino_connect:
                 caption_ok = True
                 continue
             if not caption_ok:
                 captino_connect = True
             continue
-        elif captino_connect and "caption" not in topic_data and not caption_ok:
+        elif (captino_connect and captino_long) and "caption" not in topic_data and not caption_ok and len(line) < 22:
             caption = caption + line
-            captino_connect = False
-            caption_ok = True
+            if len(line) < 22:
+                captino_connect = False
+                caption_ok = True
             continue
-        elif len(caption) > 1 and "caption" not in topic_data:
-            topic_data["id"] = caption.split(")")[0] + ")"
+        elif captino_long and "caption" not in topic_data and not caption_ok and len(line) < len(doc[ct - 1]) and not org_line.startswith(" "):
+            ll = len(doc[ct - 1])
+            caption = caption + line
+            if len(line) < 20:
+                captino_long = False
+                caption_ok = True
+            continue
+        elif len(caption) > 1 and "caption" not in topic_data and not null_caption:
+#            topic_data["id"] = caption.split(")")[0] + ")"
             topic_data["caption"] = caption.split(")")[-1]
             caption = ""
             caption_ok = False
             captino_connect = False
+            captino_long = False
             explanation = explanation + line
+            explanation_ok = True
 #        elif len(caption) <= 10 and "caption" not in topic_data:
 #            caption = caption + line
 #            captino_connect = False
 #            caption_ok = True
             continue
         elif explanation:
-            if len(line) < 14:
-                if len(line) < 2:
+            if len(line) - alpha_count(line)/2 < summary_len:
+                if len(line) < 1:
                     if not summary_start and not summary:
                         summary_start = True
+                    elif len(summary) < 20:
+                        summary_start = False
+                        summary = ""
                     else:
                         summary_start = False
+                        if summary:
+                            summary_find = True
                     continue
                 if summary_start:
                     summary = summary + line
@@ -324,9 +415,21 @@ def jsoc_report_split(file_name):
             else:
                 summary_start = False
             explanation = explanation + line
-        if line.startswith("SoC") and SoCtopic:
+        elif null_caption and len(line) > len(doc[ct - 1]) > 1:
+#            topic_data["id"] = caption
+            topic_data["caption"] = doc[ct - 1]
+            caption = ""
+            caption_ok = False
+            captino_connect = False
+            captino_long = False
+            explanation = explanation + line
+            null_caption = False
+        if abstract:
+            abstract = abstract + line
+            continue
+        if line.startswith("SoC") and soctopic:
             SoClink.append(org_line)
-        if line.startswith("P") and SoCtopic:
+        if line.startswith("P") and soctopic:
             link.append(org_line)
     result["summary"] = summary
     result["soc_link"] = SoClink
